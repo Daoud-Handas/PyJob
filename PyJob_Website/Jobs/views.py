968340ee4de95548b494a338e330350679c1348e
Daoud Handas <57755047+Daoud-Handas@users.noreply.github.com>
2022-02-20 09:51:17 +0100

    Update views.py

--- PyJob_Website/Jobs/views.py
+++ PyJob_Website/Jobs/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from indeed import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Jobs index.")
+    template = 'https://fr.indeed.com/jobs?q={}&l={}'
+    url = template.format('data scientist', 'Paris')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    cards = soup.find_all('div', 'job_seen_beacon')
+
+    jobs = []
+    for card in cards:
+        job = get_data_job(card)
+        jobs.append(job)
+    return render(request, "index.html", context={'jobs' : jobs})
