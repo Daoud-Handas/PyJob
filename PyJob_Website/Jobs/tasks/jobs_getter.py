8b11189a891567cd1977dc8e23311b747a818a84
Florent <florent.compagnoni@gmail.fr>
2022-03-01 20:55:42 +0100

    job verification + safe

---
+++ PyJob_Website/Jobs/tasks/jobs_getter.py
@@ -0,0 +1,34 @@
+from .welcome_to_jungle import scrap_welcome_to_the_jungle
+from .scrap_monster import scrap_monster_jobs
+from ..models import Job
+
+
+def job_exist(url):
+    database_job = Job.objects.filter(url=url)
+    if len(database_job) == 0:
+        return False
+    else:
+        return True
+
+
+def verify(jobs):
+    return [job for job in jobs if not job_exist(job["url"])]
+
+
+def save_jobs(jobs):
+    for job in jobs:
+        new_job = Job(**job)
+        new_job.save()
+        print(f"job ajouté : {job['url']}")
+
+
+def job_caller():
+    jobs = list()
+    jobs += scrap_welcome_to_the_jungle()
+    jobs += scrap_monster_jobs()
+    # TODO indeed
+
+    jobs = verify(jobs)
+    print("job verifiés")
+    save_jobs(jobs)
+    print("end !")
